ff2b205e015834af29f1a1f9575343066cf57544
Gurmeet Singh <scifigurmeet@gmail.com>
2022-03-14 21:23:27 +0530

    New Logic

--- ambientPickListProcessing.py
+++ ambientPickListProcessing.py
@@ -3,6 +3,7 @@ import pandas as pd
 from io import BytesIO
 import base64
 import time
+import math
 
 def get_col_widths(dataframe):
     # First we find the maximum length of the index column
@@ -15,32 +16,74 @@ def get_col_widths(dataframe):
     ]
 
 
-def to_excel(df):
+def to_excel(df, text):
+    global number
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, index=False, sheet_name='Sheet1')
     workbook = writer.book
-    worksheet = writer.sheets['Sheet1']
-    format = workbook.add_format({
-        "border": 1,
-        "border_color": "black",
-        "bold": True,
-        "align": "center",
-        "valign": "vcenter",
-    })
-    worksheet.set_default_row(height=31.5)
-    for i, width in enumerate(get_col_widths(df)):
-        worksheet.set_column(i - 1, i - 1, width + 2, format)
+    numberOfSheets = math.ceil(df.shape[0] / 12)
+    for i in range(1, numberOfSheets + 1):
+        number += 1
+        sheetName = 'Sheet ' + str(i)
+        df[(i - 1) * 12:i * 12].to_excel(writer,
+                                         index=False,
+                                         sheet_name=sheetName,
+                                         startrow=1)
+        worksheet = writer.sheets[sheetName]
+        worksheet.merge_range(
+            'A1:D1', f'AMBIENT - {text}',
+            workbook.add_format({
+                'bold': True,
+                "border": 1,
+                "border_color": "black",
+                'font_size': 14,
+                "border": 1,
+                'align': 'center',
+                'valign': 'vcenter'
+            }))
+        worksheet.merge_range(
+            'E1:F1', f'{time.strftime("%d-%m-%Y", time.localtime())}',
+            workbook.add_format({
+                'bold': True,
+                "border": 1,
+                "border_color": "black",
+                'font_size': 14,
+                'align': 'center',
+                'valign': 'vcenter'
+            }))
+        worksheet.merge_range(
+            'G1:H1', f'{number}',
+            workbook.add_format({
+                'bold': True,
+                "border": 1,
+                "border_color": "black",
+                'font_size': 14,
+                'align': 'center',
+                'valign': 'vcenter'
+            }))
+        format = workbook.add_format({
+            "border": 1,
+            "border_color": "black",
+            "bold": True,
+            "align": "center",
+            "valign": "vcenter",
+        })
+        worksheet.set_default_row(height=31.5)
+        for i, width in enumerate(get_col_widths(df)):
+            worksheet.set_column(i - 1, i - 1, width + 2, format)
     writer.save()
     processed_data = output.getvalue()
     return processed_data
 
 
-fmpMasterFile = pd.read_excel(
-    "AMBIENT_MASTER_DATA_FILE.xlsx",
-    engine="openpyxl")
+fmpMasterFile = pd.read_excel("AMBIENT_MASTER_DATA_FILE.xlsx",
+                              engine="openpyxl")
 
 st.write("""# Ambient Pick List Processing""")
+
+number = st.number_input("Enter Last Page Number",
+                         min_value=1,
+                         max_value=1000000000)
 fmpPicklist = st.file_uploader("Choose Ambient Picklist")
 
 if st.button("Process Picklist"):
@@ -61,7 +104,8 @@ if st.button("Process Picklist"):
 
         fmpPickList = fmpPickList[[
             "SingleItemOrderIDList", "MultiItemOrderIDList", "ProductID",
-            "Product Group/Type", "Qty", "Color", "Size", "SingleOrderItemCount", "MultiOrderItemCount"
+            "Product Group/Type", "Qty", "Color", "Size",
+            "SingleOrderItemCount", "MultiOrderItemCount"
         ]]
 
         #st.dataframe(fmpPickList)
@@ -72,7 +116,7 @@ if st.button("Process Picklist"):
         for index, row in fmpPickList.iterrows():
             st.text(f"Processing {row['ProductID']}")
             fmpMasterFileRow = fmpMasterFile.loc[fmpMasterFile["ProductID"] ==
-                                                row["ProductID"]]
+                                                 row["ProductID"]]
             Type = fmpMasterFileRow["ProductGroupName"].values[0]
             Size = fmpMasterFileRow["SIZE"].values[0]
             Color = fmpMasterFileRow["COLOR"].values[0]
@@ -87,7 +131,8 @@ if st.button("Process Picklist"):
 
         fmpPickList.drop(columns=["ProductID"], inplace=True)
 
-        fmpPickList = fmpPickList.rename(columns={"Product Group/Type": "Type"})
+        fmpPickList = fmpPickList.rename(
+            columns={"Product Group/Type": "Type"})
 
         fmpPickList["Cutter"] = ""
         fmpPickList["Inspection"] = ""
@@ -106,25 +151,36 @@ if st.button("Process Picklist"):
             ["NYLON", "BCF", "FLORIDA"])]
         fmpCustomList = fmpPickList.loc[~fmpPickList["Type"].
                                         isin(["NYLON", "BCF", "FLORIDA"])]
-        fmpCustomSingleOrdersList = fmpCustomList.loc[fmpCustomList["SingleItemOrderIDList"].notna()]
-        fmpCustomMultiOrdersList = fmpCustomList.loc[fmpCustomList["MultiItemOrderIDList"].notna()]
+        fmpCustomSingleOrdersList = fmpCustomList.loc[
+            fmpCustomList["SingleItemOrderIDList"].notna()]
+        fmpCustomMultiOrdersList = fmpCustomList.loc[
+            fmpCustomList["MultiItemOrderIDList"].notna()]
 
-        fmpCustomSingleOrdersList["Qty"] = fmpCustomSingleOrdersList["SingleOrderItemCount"]
-        fmpCustomMultiOrdersList["Qty"] = fmpCustomMultiOrdersList["MultiOrderItemCount"]
+        fmpCustomSingleOrdersList["Qty"] = fmpCustomSingleOrdersList[
+            "SingleOrderItemCount"]
+        fmpCustomMultiOrdersList["Qty"] = fmpCustomMultiOrdersList[
+            "MultiOrderItemCount"]
 
-        fmpCustomSingleOrdersList.rename(columns={"SingleItemOrderIDList": "OrderID"},
-                                        inplace=True)
-        fmpCustomMultiOrdersList.rename(columns={"MultiItemOrderIDList": "OrderID"},
-                                        inplace=True)
-        fmpCustomSingleOrdersList.drop(columns=["MultiItemOrderIDList"], inplace=True)
-        fmpCustomMultiOrdersList.drop(columns=["SingleItemOrderIDList"], inplace=True)
-        fmpCustomSingleOrdersList.drop(columns=["SingleOrderItemCount", "MultiOrderItemCount"], inplace=True)
-        fmpCustomMultiOrdersList.drop(columns=["SingleOrderItemCount", "MultiOrderItemCount"], inplace=True)
+        fmpCustomSingleOrdersList.rename(
+            columns={"SingleItemOrderIDList": "OrderID"}, inplace=True)
+        fmpCustomMultiOrdersList.rename(
+            columns={"MultiItemOrderIDList": "OrderID"}, inplace=True)
+        fmpCustomSingleOrdersList.drop(columns=["MultiItemOrderIDList"],
+                                       inplace=True)
+        fmpCustomMultiOrdersList.drop(columns=["SingleItemOrderIDList"],
+                                      inplace=True)
+        fmpCustomSingleOrdersList.drop(
+            columns=["SingleOrderItemCount", "MultiOrderItemCount"],
+            inplace=True)
+        fmpCustomMultiOrdersList.drop(
+            columns=["SingleOrderItemCount", "MultiOrderItemCount"],
+            inplace=True)
 
         fmpCustomSingleOrdersSmallSizesList = fmpCustomSingleOrdersList.loc[
             fmpCustomSingleOrdersList["Size"].isin([
-                "2' ROUND", "3' ROUND", "4' ROUND", "2' X 3'", "2' X 4'", "2' X 6'", "3' X 5'", "4' X 6'",
-                '18" X 36" HALF ROUND', '20" X 40" HALF ROUND', "1.5' X 2.25'"
+                "2' ROUND", "3' ROUND", "4' ROUND", "2' X 3'", "2' X 4'",
+                "2' X 6'", "3' X 5'", "4' X 6'", '18" X 36" HALF ROUND',
+                '20" X 40" HALF ROUND', "1.5' X 2.25'"
             ])]
 
         fmpCustomSingleOrdersSmallSizesListSorted = fmpCustomSingleOrdersSmallSizesList.sort_values(
@@ -148,19 +204,20 @@ if st.button("Process Picklist"):
                 "Size",
             ])
 
-        fmpCustomMultiOrdersList = fmpCustomMultiOrdersList.sort_values(by=["OrderID"])
+        fmpCustomMultiOrdersList = fmpCustomMultiOrdersList.sort_values(
+            by=["OrderID"])
 
     t = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
 
     st.markdown(
-        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCustomSingleOrdersSmallSizesListSorted)).decode()}" download="Custom_Single_Orders_Small_Sizes_List_{t}.xlsx">✔️ Custom Single Orders Small Sizes List</a>',
+        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCustomSingleOrdersSmallSizesListSorted, "Single Orders Small Sizes")).decode()}" download="Custom_Single_Orders_Small_Sizes_List_{t}.xlsx">✔️ Custom Single Orders Small Sizes List</a>',
         unsafe_allow_html=True)
     st.markdown(
-        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCustomSingleOrdersOtherSizesListSorted)).decode()}" download="Custom_Single_Orders_Other_Sizes_List_{t}.xlsx">✔️ Custom Single Orders Other Sizes List</a>',
+        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCustomSingleOrdersOtherSizesListSorted, "Single Orders Other Sizes")).decode()}" download="Custom_Single_Orders_Other_Sizes_List_{t}.xlsx">✔️ Custom Single Orders Other Sizes List</a>',
         unsafe_allow_html=True)
     st.markdown(
-        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCustomMultiOrdersList)).decode()}" download="Custom_Multi_Orders_List_{t}.xlsx">✔️ Custom Multi Orders List</a>',
+        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCustomMultiOrdersList, "Multi Orders")).decode()}" download="Custom_Multi_Orders_List_{t}.xlsx">✔️ Custom Multi Orders List</a>',
         unsafe_allow_html=True)
-    st.markdown(
-        f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCutPiecesList)).decode()}" download="Cut_Pieces_List_{t}.xlsx">✔️ Cut Pieces List</a>',
-        unsafe_allow_html=True)
+    # st.markdown(
+    #     f'<a href="data:application/octet-stream;base64,{base64.b64encode(to_excel(fmpCutPiecesList, "Cut Pieces")).decode()}" download="Cut_Pieces_List_{t}.xlsx">✔️ Cut Pieces List</a>',
+    #     unsafe_allow_html=True)
